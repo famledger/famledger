76ab6186781377c99f4dc24de89cbeccf1ee3c38
famledger <famledger@proton.me>
2024-01-03 15:01:54 -0500

    Extend InvoiceAssociationListener to handle invoice references in different formats (uuid and series-number)

--- src/EventListener/InvoiceAssociationListener.php
+++ src/EventListener/InvoiceAssociationListener.php
@@ -5,6 +5,7 @@ namespace App\EventListener;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 
 use App\Event\Invoice\InvoiceUpdatedEvent;
+use App\Exception\InvoiceAssociationException;
 use App\Repository\InvoiceRepository;
 
 /**
@@ -18,6 +19,9 @@ class InvoiceAssociationListener
     ) {
     }
 
+    /**
+     * @throws InvoiceAssociationException
+     */
     public function onInvoiceUpdated(InvoiceUpdatedEvent $event): void
     {
         $invoice = $event->getInvoice();
@@ -31,8 +35,26 @@ class InvoiceAssociationListener
         switch($relacion['tipoRelacion']) {
 
             case '04':
-                $uuid               = $relacion['Comprobantes'][0];
-                $substitutedInvoice = $this->invoiceRepository->findOneBy(['uuid' => $uuid]);
+                $uuid = $relacion['Comprobantes'][0];
+                // check whether the uuid has the form 'ED9B09C8-4A74-4DEA-80AB-889009A23C90'
+                if (preg_match('/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i', $uuid)) {
+                    // lookup by uuid
+                    $substitutedInvoice = $this->invoiceRepository->findOneBy(['uuid' => $uuid]);
+                } elseif (preg_match('/^[A-F]+-[0-9]+$/i', $uuid)) {
+                    // lookup by series and number
+                    [$series, $number] = explode('-', $uuid);
+                    $substitutedInvoice = $this->invoiceRepository->findOneBy([
+                        'series' => $series,
+                        'number' => $number
+                    ]);
+                } else {
+                    throw new InvoiceAssociationException("Invoice association error. Unsupported uuid format: '$uuid'");
+                }
+
+                if (null === $substitutedInvoice) {
+                    throw new InvoiceAssociationException("Invoice association error. Couldn't find related invoice by reference: '$uuid'");
+                }
+
                 $substitutedInvoice->setSubstitutedByInvoice($invoice);
                 break;
 
---
+++ src/Exception/InvoiceAssociationException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Exception;
+
+class InvoiceAssociationException extends \Exception
+{
+}
